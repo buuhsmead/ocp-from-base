428339698f205c5929c3fee3aa035e1a83a67d31
Huub Daems <buuhsmead@gmail.com>
2019-01-31 21:59:51 +0100

    Next commit

--- extensions/install.sh
+++ extensions/install.sh
@@ -9,3 +9,5 @@ injected_dir=$1
 source /usr/local/s2i/install-common.sh
 install_modules ${injected_dir}/modules
 configure_drivers ${injected_dir}/drivers.env
+
+mkdir -p /home/jboss/source/data/databases/derby
